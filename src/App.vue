dc2885e63284bbf81c19d94a636ba2e48cd55de3
student8 <student8@pcoip.ki.agh.edu.pl>
2022-05-28 11:51:30 +0200

    Reusing components

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div>
     <h1>Witaj w systemie do zapisów na zajęcia</h1>
     <div v-if="!isAuthenticated">
-      <login-form @login="logIn($event)"></login-form>
+      <login-form @login="logIn($event)" button-label="Wejdź"></login-form>
+      <login-form @login="logIn($event)" button-label="Wleć"></login-form>
+      <login-form @login="logIn($event)" :button-label="Math.random() < 0.5 ? 'Etykieta A' : 'Etykieta B'"></login-form>
     </div>
     <div v-else>
       <h2>Witaj {{email}}</h2>
